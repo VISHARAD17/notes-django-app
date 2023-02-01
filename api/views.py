36fee61fdd5a1de666bd85b2d8aa80be9da38d85
visharad <visharadborsutkar777@gmail.com>
2023-02-01 15:05:22 +0530

    init

---
+++ api/views.py
@@ -0,0 +1,71 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .utils import updateNote, getNoteDetail, deleteNote, getNotesList, createNote
+# Create your views here.
+
+
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting note'
+        },
+    ]
+    return Response(routes)
+
+
+# /notes GET --> get all notes
+# /notes POST --> create a note
+# /notes/<id> GET --> get a single note using its id
+# /notes/<id> PUT --> update a part of the select note 
+# /notes/<id> DELETE --> delete the selected note
+
+@api_view(['GET', 'POST'])
+def getNotes(request):
+
+    if request.method == 'GET':
+        return getNotesList(request)
+
+    if request.method == 'POST':
+        return createNote(request)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def getNote(request, pk):
+
+    if request.method == 'GET':
+        return getNoteDetail(request, pk)
+
+    if request.method == 'PUT':
+        return updateNote(request, pk)
+
+    if request.method == 'DELETE':
+        return deleteNote(request, pk)
